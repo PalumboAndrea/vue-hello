7672f42acdbe25a86298f7ebc9d4b5794473d1e3
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-12 15:48:25 +0100

    bonus - add image

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
 createApp({
     data()  {
         return {
-            message:'Hello Vue!'
+            message:'Hello Vue!',
+            url: 'https://i.pinimg.com/originals/da/7b/61/da7b615cd6bf4db75fe36ab49d11c69e.jpg',
+            size: 'sprinter-size',
         }
     }
 }).mount('#app')
