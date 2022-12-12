8e9633ec19e66e6dfc540e0d3f8a5779678a2996
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-12 16:10:05 +0100

    add functionality input

--- js/myscript.js
+++ js/myscript.js
@@ -5,7 +5,7 @@ createApp({
         return {
             message:'Hello Vue!',
             url: 'https://i.pinimg.com/originals/da/7b/61/da7b615cd6bf4db75fe36ab49d11c69e.jpg',
-            size: 'sprinter-size',
+            size: ['sprinter-size', 'px-4', 'mt-2'],
         }
     }
 }).mount('#app')
